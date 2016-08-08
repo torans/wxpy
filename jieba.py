c1651102a9c68d0e2f1881110423f8d57f1582f2
torans <504788375@qq.com>
2016-08-08 11:37:10 +0800

    添加结巴注释信息

--- jieba.py
+++ jieba.py
@@ -1,6 +1,17 @@
 # encoding=utf-8
 import jieba
 
+"""
+1.结巴分词模块安装方法：pip install jieba / easy_install jieba
+2.jieba.cut 方法接受三个输入参数: 需要分词的字符串；cut_all 参数用来控制是否采用全模式；HMM 参数用来控制是否使用 HMM 模型
+3.jieba.cut_for_search 方法接受两个参数：需要分词的字符串；是否使用 HMM 模型。该方法适合用于搜索引擎构建倒排索引的分词，粒度比较细
+待分词的字符串可以是 unicode 或 UTF-8 字符串、GBK 字符串。注意：不建议直接输入 GBK 字符串，可能无法预料地错误解码成 UTF-8
+4.jieba.cut 以及 jieba.cut_for_search 返回的结构都是一个可迭代的 generator，可以使用 for 循环来获得分词后得到的每一个词语(unicode)，或者用
+5.jieba.lcut 以及 jieba.lcut_for_search 直接返回 list
+6.jieba.Tokenizer(dictionary=DEFAULT_DICT) 新建自定义分词器，可用于同时使用不同词典。jieba.dt 为默认分词器，所有全局分词相关函数都是该分词器的映射。
+
+"""
+
 seg_list = jieba.cut("大数据的关键在于数据收集和分析", cut_all=True)
 print("Full Mode: " + "/ ".join(seg_list))  # 全模式
 
